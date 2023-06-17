a0885d3301fbe6c76b1e999811837bbd5d770f5a
QINGCHARLES <kingcharlesother@gmail.com>
2023-06-17 15:55:29 -0500

    .

---
+++ TaxAppeal/Models/GisAddressPin.cs
@@ -0,0 +1,68 @@
+﻿namespace TaxAppeal.Models
+{
+	public class Candidate
+	{
+		public string? address { get; set; }
+		public Location? location { get; set; }
+		public int score { get; set; }
+		public Attributes? attributes { get; set; }
+	}
+
+	public class Location
+	{
+		public double x { get; set; }
+		public double y { get; set; }
+	}
+
+	public class GisAddressPin
+	{
+		public SpatialReference? spatialReference { get; set; }
+		public List<Candidate>? candidates { get; set; }
+	}
+
+	public class SpatialReference
+	{
+		public int wkid { get; set; }
+		public int latestWkid { get; set; }
+	}
+
+	// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+	public class Attributes
+	{
+		public string? PIN14 { get; set; }
+	}
+
+	public class Feature
+	{
+		public Attributes? attributes { get; set; }
+		public Geometry? geometry { get; set; }
+	}
+
+	public class Field
+	{
+		public string? name { get; set; }
+		public string? type { get; set; }
+		public string? alias { get; set; }
+		public int? length { get; set; }
+	}
+
+	public class FieldAliases
+	{
+		public string? PIN14 { get; set; }
+	}
+
+	public class Geometry
+	{
+		public List<List<List<double>>>? rings { get; set; }
+	}
+
+	public class GisPin
+	{
+		public string? displayFieldName { get; set; }
+		public FieldAliases? fieldAliases { get; set; }
+		public string? geometryType { get; set; }
+		public SpatialReference? spatialReference { get; set; }
+		public List<Field>? fields { get; set; }
+		public List<Feature>? features { get; set; }
+	}
+}
--- TaxAppeal/Pages/Index.cshtml.cs
+++ TaxAppeal/Pages/Index.cshtml.cs
@@ -4,40 +4,80 @@ using System.Net;
 using HtmlAgilityPack;
 using Dapper;
 using System.Web;
-using Microsoft.Data.SqlClient;
-using System.Security.Policy;
-using System.Net;
+using System.Text.Json;
+using TaxAppeal.Models;
+using System.Globalization;
+using System.Text.Encodings.Web;
 
-namespace TaxAppeal.Pages
+namespace TaxAppeal.Pages;
+
+public class IndexModel : PageModel
 {
-	public class IndexModel : PageModel
+
+	[BindProperty]
+	public string? StreetAddress { get; set; }
+
+	private readonly ILogger<IndexModel> _logger;
+
+	public IndexModel(ILogger<IndexModel> logger)
 	{
-		private readonly ILogger<IndexModel> _logger;
+		_logger = logger;
+	}
 
-		public IndexModel(ILogger<IndexModel> logger)
+	//		https://gis.cookcountyil.gov/traditional/rest/services/cookVwrDynmc/MapServer/44/query?f=json&where=&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry=%7B%22points%22%3A%5B%5B1147343.1412663457%2C1927226.7877129088%5D%5D%2C%22spatialReference%22%3A%7B%22wkid%22%3A3435%7D%7D&geometryType=esriGeometryMultipoint&inSR=3435&outFields=PIN14&outSR=3435
+
+	//https://gis.cookcountyil.gov/traditional/rest/services/cookVwrDynmc/MapServer/44/query?f=json&where=&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry=%7B%22points%22%3A%5B%5B1147343.1412663457%2C1927226.7877129088%5D%5D%2C%22spatialReference%22%3A%7B%22wkid%22%3A3435%7D%7D&geometryType=esriGeometryMultipoint&inSR=3435&outFields=PIN14&outSR=3435
+
+
+	//https://gis.cookcountyil.gov/traditional/rest/services/cookVwrDynmc/MapServer/44/query?f=json&where=&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry={"points":[[1147343.1412663457,1927226.7877129088]],"spatialReference":{"wkid":3435}}&geometryType=esriGeometryMultipoint&inSR=3435&outFields=ParcelType,Address,City,Town,NBHD,TotalValue,BldgValue,BLDGClass,BldgSqft,Landvalue,LandSqft,BldgConst,BldgAge,Mlt_IND,Per_Ass,PIN10,PIN14,SV_URL,TAXYR,value_description&outSR=3435
+
+
+	public async Task<IActionResult> OnPost()
+	{
+		string fff = "address not found";
+
+
+		if (!string.IsNullOrWhiteSpace(StreetAddress))
 		{
-			_logger = logger;
-		}
 
-//		https://gis.cookcountyil.gov/traditional/rest/services/cookVwrDynmc/MapServer/44/query?f=json&where=&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry=%7B%22points%22%3A%5B%5B1147343.1412663457%2C1927226.7877129088%5D%5D%2C%22spatialReference%22%3A%7B%22wkid%22%3A3435%7D%7D&geometryType=esriGeometryMultipoint&inSR=3435&outFields=PIN14&outSR=3435
-
-//https://gis.cookcountyil.gov/traditional/rest/services/cookVwrDynmc/MapServer/44/query?f=json&where=&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry=%7B%22points%22%3A%5B%5B1147343.1412663457%2C1927226.7877129088%5D%5D%2C%22spatialReference%22%3A%7B%22wkid%22%3A3435%7D%7D&geometryType=esriGeometryMultipoint&inSR=3435&outFields=PIN14&outSR=3435
-
-
-//https://gis.cookcountyil.gov/traditional/rest/services/cookVwrDynmc/MapServer/44/query?f=json&where=&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry={"points":[[1147343.1412663457,1927226.7877129088]],"spatialReference":{"wkid":3435}}&geometryType=esriGeometryMultipoint&inSR=3435&outFields=ParcelType,Address,City,Town,NBHD,TotalValue,BldgValue,BLDGClass,BldgSqft,Landvalue,LandSqft,BldgConst,BldgAge,Mlt_IND,Per_Ass,PIN10,PIN14,SV_URL,TAXYR,value_description&outSR=3435
-
-
-		public IActionResult OnGet()
-		{
-			string fff = "";
-			using (var client = new WebClient())
+			using HttpClient client = new()
 			{
-				string Url = "https://gis.cookcountyil.gov/traditional/rest/services/cookVwrDynmc/MapServer/44/query?where=&text=&objectIds=&time=&timeRelation=esriTimeRelationOverlaps&geometry=1147343.1412663457%2C1927226.7877129088&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=PIN14&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&havingClause=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&historicMoment=&returnDistinctValues=false&resultOffset=&resultRecordCount=&returnExtentOnly=false&sqlFormat=none&datumTransformation=&parameterValues=&rangeValues=&quantizationParameters=&featureEncoding=esriDefault&f=pjson";
+				BaseAddress = new Uri("https://gis.cookcountyil.gov")
+			};
 
-				fff = client.DownloadString(Url);
+			//fff = await client.GetStringAsync()
+
+			GisAddressPin? JsonAddress = await client.GetFromJsonAsync<GisAddressPin>($"/traditional/rest/services/AddressLocator/addressPtMuniZip/GeocodeServer/findAddressCandidates?Street={HttpUtility.UrlEncode(StreetAddress)}&f=json");
+
+			if (JsonAddress != null && JsonAddress.candidates != null && JsonAddress.candidates.Count > 0)
+			{
+				//fff = JsonAddress.candidates[0].location!.x.ToString();
+				//using (var clientb = new WebClient())
+				//{
+				//	// 
+				//	string Url = $"https://gis.cookcountyil.gov/traditional/rest/services/cookVwrDynmc/MapServer/44/query?where=&text=&objectIds=&time=&timeRelation=esriTimeRelationOverlaps&geometry={JsonAddress.candidates[0].location!.x.ToString()}%2C{JsonAddress.candidates[0].location!.y.ToString()}&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=PIN14&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&havingClause=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&historicMoment=&returnDistinctValues=false&resultOffset=&resultRecordCount=&returnExtentOnly=false&sqlFormat=none&datumTransformation=&parameterValues=&rangeValues=&quantizationParameters=&featureEncoding=esriDefault&f=pjson";
+
+				//	fff = clientb.DownloadString(Url);
+				//}
+
+				GisPin? JsonGisPin = await client.GetFromJsonAsync<GisPin>($"traditional/rest/services/cookVwrDynmc/MapServer/44/query?where=&text=&objectIds=&time=&timeRelation=esriTimeRelationOverlaps&geometry={JsonAddress.candidates[0].location!.x.ToString()}%2C{JsonAddress.candidates[0].location!.y.ToString()}&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=PIN14&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&havingClause=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&historicMoment=&returnDistinctValues=false&resultOffset=&resultRecordCount=&returnExtentOnly=false&sqlFormat=none&datumTransformation=&parameterValues=&rangeValues=&quantizationParameters=&featureEncoding=esriDefault&f=pjson");
+
+				fff = "Your property PIN is: " + JsonGisPin!.features![0].attributes!.PIN14!;
 			}
 
-			return Content(fff);
+
+			//Console.WriteLine($"Id: {user?.Id}");
+			//Console.WriteLine($"Name: {user?.Name}");
+			//Console.WriteLine($"Username: {user?.Username}");
+			//Console.WriteLine($"Email: {user?.Email}");
+
+
 		}
+
+
+
+
+
+		return Content(fff);
 	}
 }
