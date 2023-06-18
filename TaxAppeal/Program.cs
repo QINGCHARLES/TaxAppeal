d54fceb0b66639b7f5c10c54267dbcbf66f6dc29
QINGCHARLES <kingcharlesother@gmail.com>
2023-06-18 15:49:00 -0500

    .

--- TaxAppeal/Program.cs
+++ TaxAppeal/Program.cs
@@ -4,6 +4,8 @@ using TaxAppeal.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseSystemd();
+
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
