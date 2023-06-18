1009b61522e9d9e413b18aa766093864d147e4c9
QINGCHARLES <kingcharlesother@gmail.com>
2023-06-18 15:38:03 -0500

    .

--- TaxAppeal/Pages/Index.cshtml.cs
+++ TaxAppeal/Pages/Index.cshtml.cs
@@ -34,10 +34,18 @@ public class IndexModel : PageModel
 
 	public async Task<IActionResult> OnPost()
 	{
+		if (!ModelState.IsValid)
+		{
+			return Page();
+		}
+
+
 		string fff = "address not found";
 
+		// this url is for a lookahead address validation if needed
 		// https://gis.cookcountyil.gov/traditional/rest/services/AddressLocator/addressPtMuniZip/GeocodeServer/findAddressCandidates?Street=4127%20n%20tripp%20ave&f=json
 
+		// Make sure there is some text entered in the textbox
 		if (!string.IsNullOrWhiteSpace(StreetAddress))
 		{
 
@@ -48,8 +56,10 @@ public class IndexModel : PageModel
 
 			//fff = await client.GetStringAsync()
 
+			// this call converts a street address text to x/y coords for the other functions
 			GisAddressPin? JsonAddress = await client.GetFromJsonAsync<GisAddressPin>($"/traditional/rest/services/AddressLocator/addressPtMuniZip/GeocodeServer/findAddressCandidates?Street={HttpUtility.UrlEncode(StreetAddress)}&f=json");
 
+			// check to see if the street address entered on the form validated to a street address in the county database
 			if (JsonAddress != null && JsonAddress.candidates != null && JsonAddress.candidates.Count > 0)
 			{
 				//fff = JsonAddress.candidates[0].location!.x.ToString();
@@ -61,16 +71,13 @@ public class IndexModel : PageModel
 				//	fff = clientb.DownloadString(Url);
 				//}
 
+				// this call converts an x/y coord to a property PIN
 				GisPin? JsonGisPin = await client.GetFromJsonAsync<GisPin>($"traditional/rest/services/cookVwrDynmc/MapServer/44/query?where=&text=&objectIds=&time=&timeRelation=esriTimeRelationOverlaps&geometry={JsonAddress.candidates[0].location!.x.ToString()}%2C{JsonAddress.candidates[0].location!.y.ToString()}&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=PIN14&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&havingClause=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&historicMoment=&returnDistinctValues=false&resultOffset=&resultRecordCount=&returnExtentOnly=false&sqlFormat=none&datumTransformation=&parameterValues=&rangeValues=&quantizationParameters=&featureEncoding=esriDefault&f=pjson");
 
 				fff = "Your property PIN is: " + JsonGisPin!.features![0].attributes!.PIN14!;
 			}
 
 
-			//Console.WriteLine($"Id: {user?.Id}");
-			//Console.WriteLine($"Name: {user?.Name}");
-			//Console.WriteLine($"Username: {user?.Username}");
-			//Console.WriteLine($"Email: {user?.Email}");
 
 
 		}
