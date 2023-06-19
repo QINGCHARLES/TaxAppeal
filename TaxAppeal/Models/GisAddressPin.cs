f397a34effc488dfa243ce822e35fbf53cedf105
QINGCHARLES <kingcharlesother@gmail.com>
2023-06-19 18:27:53 -0500

    .

--- TaxAppeal/Models/GisAddressPin.cs
+++ TaxAppeal/Models/GisAddressPin.cs
@@ -26,11 +26,6 @@
 		public int latestWkid { get; set; }
 	}
 
-	// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
-	public class Attributes
-	{
-		public string? PIN14 { get; set; }
-	}
 
 	public class Feature
 	{
@@ -38,17 +33,13 @@
 		public Geometry? geometry { get; set; }
 	}
 
+
 	public class Field
 	{
 		public string? name { get; set; }
 		public string? type { get; set; }
 		public string? alias { get; set; }
 		public int? length { get; set; }
-	}
-
-	public class FieldAliases
-	{
-		public string? PIN14 { get; set; }
 	}
 
 	public class Geometry
@@ -65,4 +56,72 @@
 		public List<Field>? fields { get; set; }
 		public List<Feature>? features { get; set; }
 	}
+
+	// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+	public class Attributes
+	{
+		public object? PARCELTYPE { get; set; }
+		public string? Address { get; set; }
+		public string? City { get; set; }
+		public string? Town { get; set; }
+		public string? NBHD { get; set; }
+		public string? TotalValue { get; set; }
+		public string? BldgValue { get; set; }
+		public string? BLDGClass { get; set; }
+		public int? BldgSqft { get; set; }
+		public string? LandValue { get; set; }
+		public double? LandSqft { get; set; }
+		public string? BldgConst { get; set; }
+		public int? BldgAge { get; set; }
+		public object? MLT_IND { get; set; }
+		public object? Per_Ass { get; set; }
+		public string? Pin10 { get; set; }
+		public string? PIN14 { get; set; }
+		public string? SV_URL { get; set; }
+		public int? TAXYR { get; set; }
+		public string? value_description { get; set; }
+	}
+
+
+
+
+
+	public class FieldAliases
+	{
+		public string? PARCELTYPE { get; set; }
+		public string? Address { get; set; }
+		public string? City { get; set; }
+		public string? Town { get; set; }
+		public string? NBHD { get; set; }
+		public string? TotalValue { get; set; }
+		public string? BldgValue { get; set; }
+		public string? BLDGClass { get; set; }
+		public string? BldgSqft { get; set; }
+		public string? LandValue { get; set; }
+		public string? LandSqft { get; set; }
+		public string? BldgConst { get; set; }
+		public string? BldgAge { get; set; }
+		public string? MLT_IND { get; set; }
+		public string? Per_Ass { get; set; }
+		public string? Pin10 { get; set; }
+		public string? PIN14 { get; set; }
+		public string? SV_URL { get; set; }
+		public string? TAXYR { get; set; }
+		public string? value_description { get; set; }
+	}
+
+
+
+	public class GisComparables
+	{
+		public string? displayFieldName { get; set; }
+		public FieldAliases? fieldAliases { get; set; }
+		public string? geometryType { get; set; }
+		public SpatialReference? spatialReference { get; set; }
+		public List<Field>? fields { get; set; }
+		public List<Feature>? features { get; set; }
+	}
+
+
+
 }
--- TaxAppeal/Pages/StepTwo.cshtml.cs
+++ TaxAppeal/Pages/StepTwo.cshtml.cs
@@ -1,5 +1,10 @@
+using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Web;
+using TaxAppeal.Models;
 
 namespace TaxAppeal.Pages;
 
@@ -9,7 +14,34 @@ public class StepTwoModel : PageModel
 	public string? PropertyPin { get; set; }
 
 
-	public void OnGet()
-    {
-    }
+	public async Task<IActionResult> OnGetAsync()
+	{
+		using HttpClient client = new()
+		{
+			BaseAddress = new Uri("https://gis.cookcountyil.gov")
+		};
+		//
+
+		GisComparables? JsonComparables = await client.GetFromJsonAsync<GisComparables>($"/traditional/rest/services/cookVwrDynmc/MapServer/44/query?where=Town+%3D+%27Jefferson%27+AND+NBHD+%3D+%27074%27+AND+BLDGClass+%3D+%27203%27+AND+%28BldgSqft+%3E%3D+1190.7+AND+BldgSqft+%3C%3D+1455.3%29+AND+%28LandSqft+%3E%3D+7021.8+AND+LandSqft+%3C%3D+8582.2%29+AND+%28BldgAge+%3E%3D+87+AND+BldgAge+%3C%3D+117%29+AND+BldgConst+%3D+%27Masonry%27+AND+PIN14+%3C%3E+%2713154150070000%27&text=&objectIds=&time=&timeRelation=esriTimeRelationOverlaps&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=ParcelType%2CAddress%2CCity%2CTown%2CNBHD%2CTotalValue%2CBldgValue%2CBLDGClass%2CBldgSqft%2CLandvalue%2CLandSqft%2CBldgConst%2CBldgAge%2CMlt_IND%2CPer_Ass%2CPIN10%2CPIN14%2CSV_URL%2CTAXYR%2Cvalue_description&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&havingClause=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&historicMoment=&returnDistinctValues=false&resultOffset=&resultRecordCount=&returnExtentOnly=false&sqlFormat=none&datumTransformation=&parameterValues=&rangeValues=&quantizationParameters=&featureEncoding=esriDefault&f=pjson");
+
+		if (JsonComparables == null)
+		{
+			return Content("not found");
+		}
+		else
+		{
+			string ff = "";
+			foreach (Feature feature in JsonComparables.features!)
+			{
+
+				ff += feature.attributes!.Address + "<br />";
+				ff += feature.attributes!.PIN14 + "<br />";
+
+			}
+
+			return Content(ff);
+		}
+
+		return Page();
+	}
 }
