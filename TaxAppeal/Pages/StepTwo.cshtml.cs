06d708013b1884709c5b154aa0b577d0a1ffa870
QINGCHARLES <kingcharlesother@gmail.com>
2023-10-02 19:53:18 -0500

    f

--- TaxAppeal/Pages/StepTwo.cshtml.cs
+++ TaxAppeal/Pages/StepTwo.cshtml.cs
@@ -18,6 +18,9 @@ public class StepTwoModel : PageModel
 	public string? City { get; set; }
 	public string? ZipCode { get; set; }
 
+	public string? Name { get; set; }
+
+
 	public IActionResult OnPostCancel()
 	{
 		return Redirect("/");
